7882b1af94f6b0b499fb8727e784b4c5404e35c0
jaredaitken6 <jaredaitken38@gmail.com>
2026-07-17 04:38:42 -0400

    hover and manual size input

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 let gridContainer = document.querySelector('.grid-container');
 let gridRow;
 let squareDiv;
-let gridSquare = document.querySelector('.grid-square');
+let gridSquare;
+let gridSizeButton = document.querySelector('.grid-size-button');
+let gridSizeInput;
 
 // create grid
-for (let index = 0; index < 4; index++) {
+for (let index = 0; index < 8; index++) {
     gridRow = document.createElement('div');
     gridRow.classList.add('grid-row');
     gridContainer.appendChild(gridRow); 
 
-    for (let index = 0; index < 4; index++) {
+    for (let index = 0; index < 8; index++) {
         squareDiv = document.createElement('div');
         squareDiv.classList.add('grid-square');
         gridRow.appendChild(squareDiv); 
@@ -17,9 +19,24 @@ for (let index = 0; index < 4; index++) {
 
 }
 
-console.log(gridSquare); // is null
+// hover to change square color
+gridSquare  = document.querySelectorAll('.grid-square');
 
-// gridSquare.addEventListener('mouseenter', () => {
-//     gridSquare.classList.toggle('toggle-color'); 
-// });
-    
+gridSquare.forEach((element) => {
+    element.addEventListener('mouseenter', () => {
+        element.classList.add('toggle-color'); 
+    });
+});
+
+gridSizeButton.addEventListener('click', () => {
+        do {
+            gridSizeInput = prompt('How many squares per side?', 'Numbers 100 or below');
+        } while (gridSizeInput > 100);  
+
+
+});
+
+
+
+
+
